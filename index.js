67b85c637b3deb311e08da78af9f7c5b3f30fde0
ANAS DAVOOD TK <anasdavoodtk1@gmail.com>
2021-05-20 22:19:19 +0530

    10:19 pm 20

--- index.js
+++ index.js
@@ -1,11 +1,14 @@
 require('dotenv').config();
 const Discord = require("discord.js")
+const  MessageEmbed  = require('discord.js');
 const client = new Discord.Client()
-const fetch = require('node-fetch');
-const https = require('https');
+const request = require('request');
 let url = 'https://cdn-api.co-vin.in/api/v2/admin/location/districts/17'
 
-fetchDataAsync();
+
+let options = {json: true};
+
+//fetchDataAsync();
 
 
 client.on("ready", () => {
@@ -15,35 +18,71 @@ client.on("ready", () => {
 client.on("message", async msg => {
     mess = msg.content;
   if (mess.includes('hi')) {
-    msg.reply("Hello");
+     msg.reply("HI Choose You District");
     //const { file } = await fetch('https://aws.random.cat/meow').then(response => response.json());
    // msg.channel.send(file);
-    fetchDataAsync();
+   request(url, options, (error, res, body) => {
+    if (error) {
+        return  console.log(error)
+    };
 
+    if (!error && res.statusCode == 200) {
+        
+      //console.log("District: "+body.districts[0].district_name+" ("+" Id: "+body.districts[0].district_id+")");
+
+        var district = body.districts
+        
+        for(var i = 0; i < district.length; i++)
+        {
+         var dist_data = district[i].district_name+" ("+" Id: "+body.districts[i].district_id+")";
+         console.log(dist_data);
+        
+ 
+         const embed = new Discord.MessageEmbed()
+	        .setColor('#EFFF00')
+	        .setTitle('District List')
+      	 .addFields(
+	   	    { 'test':dist_data }, );
+
+           msg.channel.send(embed);
+
+         
+         if(i = district.length)
+         {
+           console.log(i +"  "+district.length)
+          break;
+         }
+         
+
+         }
+        //district.map(({district_name})=> console.log(district_name)) 
+    };
+  });
+   
   }
 })
 
 function fetchDataAsync() {
   
-https.get(url,(res) => {
-    let body = "";
+  request(url, options, (error, res, body) => {
+    if (error) {
+        return  console.log(error)
+    };
 
-    res.on("data", (chunk) => {
-        body += chunk;
-    });
+    if (!error && res.statusCode == 200) {
+        
+      //console.log("District: "+body.districts[0].district_name+" ("+" Id: "+body.districts[0].district_id+")");
 
-    res.on("end", () => {
-        try {
-            let json = JSON.parse(body);
-            console.log(json);
-        } catch (error) {
-            console.error(error.message);
-        };
-    });
-
-}).on("error", (error) => {
-    console.error(error.message);
+        var district = body.districts
+        for(var i = 0; i < district.length; i++)
+        {
+         var dist_data = district[i].district_name+" ("+" Id: "+body.districts[i].district_id+")";
+         console.log(dist_data);
+         }
+        //district.map(({district_name})=> console.log(district_name)) 
+    };
 });
+
 }
 
 
