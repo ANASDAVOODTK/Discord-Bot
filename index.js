5608425a03058a11f13f976dfca2a1a58aceff32
Alexy Pulivelil <alexypulivelil@gmail.com>
2021-05-21 12:31:19 +0530

    States added

--- index.js
+++ index.js
@@ -2,10 +2,11 @@ require('dotenv').config();
 const Discord = require("discord.js")
 const client = new Discord.Client()
 const request = require('request');
+let urls = 'https://cdn-api.co-vin.in/api/v2/admin/location/states'
 let url = 'https://cdn-api.co-vin.in/api/v2/admin/location/districts/17'
 
 let options = { json: true };
-
+let optionssta = { json: true};
 
 client.on("ready", () => {
   console.log('Ready');
@@ -21,15 +22,41 @@ client.on("message", async msg => {
   }
 
 
-  if (message.includes('vacin centers')) {
+  if (message.includes('vaccine')) {
     msg.reply("HI Choose You Sate");
 
-    //code for listing state (for alexy)
+    request(urls, optionssta, (error, res, body) => {
+      if (error) {
+        return console.log(error)
+      };
+
+      if (!error && res.statusCode == 200) {
+
+        var state = body.states
+
+        for (var i = 0; i < state.length; i++) {
+          var state_data = state[i].state_name.toString() + " (" + " Id: " + body.states[i].state_id.toString() + ")";
+          console.log(state_data);
+
+          msg.channel.send({
+            embed: {
+              title: `${state[i].state_name}`,
+              color: 3447003,
+              description: `${body.states[i].state_id}`
+            }
+          });
+
+
+        }
+
+      };
+    });
+
   }
 
 
   //code for listing districts in the above state
-  else if (message.includes('vacin centers')) {
+  else if (message.includes('districtid')) {
     msg.reply("HI Choose You District");
 
     request(url, options, (error, res, body) => {
