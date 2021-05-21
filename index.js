11acf6a5ba9063904e903381732ec0837720f100
ANAS DAVOOD TK <anasdavoodtk1@gmail.com>
2021-05-21 15:58:30 +0530

    commited on 21-05-21 03:58 PM

--- index.js
+++ index.js
@@ -3,10 +3,10 @@ const Discord = require("discord.js")
 const client = new Discord.Client()
 const request = require('request');
 let urls = 'https://cdn-api.co-vin.in/api/v2/admin/location/states'
-let url = 'https://cdn-api.co-vin.in/api/v2/admin/location/districts/17'
 
 let options = { json: true };
 let optionssta = { json: true};
+const prefix = "!";
 
 client.on("ready", () => {
   console.log('Ready');
@@ -42,7 +42,7 @@ client.on("message", async msg => {
           var state_data = state[i].state_name.toString() + " (" + " Id: " + body.states[i].state_id.toString() + ")";
           console.log(state_data);
 
-          num_str += state[i].state_name.toString()+ " Id: " +"!"+body.states[i].state_id.toString()
+          num_str += state[i].state_name.toString()+ " Id: " + "!"+body.states[i].state_id.toString()
 
           if(i < (arr_len-1) ){
             num_str += '\n';
@@ -60,8 +60,6 @@ client.on("message", async msg => {
           });
         }, 1 * 1000);
 
-
-
         
 
       };
@@ -71,10 +69,14 @@ client.on("message", async msg => {
 
 
   //code for listing districts in the above state
-  if (message.includes('districtid')) {
+  if (msg.content.startsWith(prefix)) {
     msg.reply("HI Choose You District");
 
-    request(url, options, (error, res, body) => {
+    const useerinput = msg.content.slice(prefix.length).trim().split(' ');
+
+    let urld = "https://cdn-api.co-vin.in/api/v2/admin/location/districts/"+useerinput
+
+    request(urld, options, (error, res, body) => {
       if (error) {
         return console.log(error)
       };
@@ -83,21 +85,34 @@ client.on("message", async msg => {
 
         var district = body.districts
 
-        for (var i = 0; i < district.length; i++) {
+        var dislength = district.length;
+
+        var dist_string = '';
+
+        for (var i = 0; i < dislength; i++) {
           var dist_data = district[i].district_name.toString() + " (" + " Id: " + body.districts[i].district_id.toString() + ")";
           console.log(dist_data);
 
+          dist_string  += district[i].district_name.toString()+ " Id: " + "!"+body.districts[i].district_id.toString()
+
+          if(i < (dislength-1) ){
+            dist_string += '\n';
+           }
+        }
+
+        setTimeout(function(){
+            
           msg.channel.send({
             embed: {
-              title: `${district[i].district_name}`,
+              title: "Choose Your District",
               color: 3447003,
-              description: `${body.districts[i].district_id}`
+              description: `${dist_string}`
             }
           });
 
+        }, 1 * 1000);
 
-        }
-
+        
       };
     });
 
